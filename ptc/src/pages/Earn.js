40f20fccc448bfd77f2669130c133e8634a924e9
EmmaTheDev01 <worldoffictionrw@gmail.com>
2024-06-25 13:58:18 +0300

    Adding a NavBar

--- ptc/src/pages/Earn.js
+++ ptc/src/pages/Earn.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import NavBar from '../components/NavBar/NavBar'
 import AdCard from '../components/Earn/AdCard'
-
+import Footer from '../components/Footer'
 const Earn = () => {
   return (
     <div>
-    <NavBar/>
-    <AdCard/>
+      <NavBar />
+      <AdCard />
+      <Footer />
     </div>
   )
 }
